242d051fef70ec0631a04c88d55f340b2adb4e07
Pat <pat@tildeteam.org>
2020-09-07 16:31:39 +0000

    add start end end commit titles

--- update.sh
+++ update.sh
@@ -5,13 +5,13 @@ date > README.md;
 time yarn >> README.md;
 time yarn run ng update >> README.md;
 time git add README.md;
-time git commit -m "check update angular" -m "just checking";
+time git commit -m "start check update angular" -m "just checking";
 time yarn run ng update @angular/core @angular/cli rxjs;
 time yarn run ng update;
 echo "please stand by..." >> README.md
 date >> README.md;
 time git add README.md;
-time git commit -m "check update angular" -m "just checking";
+time git commit -m "end check update angular" -m "just checking";
 # time yarn run ng build --prod;
 # time yarn run ng test --watch=false --codeCoverage=true;
 time git add .;
